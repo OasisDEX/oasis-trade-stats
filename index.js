c7b59971a85891766cf9c839291432ac995602ed
Pex <georgi.h.georgieff@gmail.com>
2018-03-27 09:31:56 +0200

    Handling configuration scenario of wrongful network name. Everything that is unknown will be marked as private network and will use http://localhost:8545. In order to configure it, config.json must be changed

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const config = require("./config.json");
+const Web3 = require("web3");
 
-const network = process.env.NETWORK;
-const proxyFactoryAddress = config[network].proxyFactory;
+const PRIVATE_NETWORK = "private";
+
+const network = process.env.NETWORK || DEFAULT_NETWORK;
+
+/** If unknown network is provided,
+ *  then the configuration for private network is used *
+ */
+const networkConfig = config[network] || config[PRIVATE_NETWORK];
+
+const provider = networkConfig.provider;
+const proxyFactoryAddress = networkConfig.proxyFactory;
+
+if(!proxyFactoryAddress) {
+  console.log("Missing Proxy Factory address. Check config.json!");
+  process.exit(1);
+}
