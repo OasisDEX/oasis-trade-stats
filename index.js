b86598f9fbaed05646d0f1a5c551483a491dd8d9
Pex <georgi.h.georgieff@gmail.com>
2018-03-27 12:01:27 +0200

    Listening for LogTake events

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const debug = require('debug')('runtime');
 const config = require("./config.json");
 const proxyFactoryAbi = require("./abi/dsProxyFactory.json").abi;
+const marketAbi = require("./abi/matchingMarket.json").abi;
 const Web3 = require("web3");
 
 const PRIVATE_NETWORK = "private";
@@ -11,24 +13,60 @@ const network = process.env.NETWORK || DEFAULT_NETWORK;
  */
 const networkConfig = config[network] || config[PRIVATE_NETWORK];
 
-const provider = networkConfig.provider;
+const providerAddress = networkConfig.provider;
+const marketAddress = networkConfig.market;
 const proxyFactoryAddress = networkConfig.proxyFactory;
 
-if(!proxyFactoryAddress) {
+if (!marketAddress) {
+  console.log("Missing Market address. Check config.json!");
+  process.exit(1);
+}
+
+if (!proxyFactoryAddress) {
   console.log("Missing Proxy Factory address. Check config.json!");
   process.exit(1);
 }
 
-const web3 = new Web3(provider);
-const proxyFactoryInstance = new web3.eth.Contract(proxyFactoryAbi,proxyFactoryAddress);
+const web3 = new Web3(new Web3.providers.HttpProvider(providerAddress));
 
-console.log(proxyFactoryInstance);
+const proxyFactory = web3.eth.contract(proxyFactoryAbi).at(proxyFactoryAddress);
+const market = web3.eth.contract(marketAbi).at(marketAddress);
 
-function isAddressAProxy(address){
-   return proxyFactoryInstance.methods.isProxy(address).call();
+function isAddressAProxy(address) {
+  return new Promise((resolve, reject) => {
+    proxyFactory.isProxy(address, (err, isProxy) => {
+      if (!err) {
+        resolve(isProxy);
+      } else {
+        debug(`Couldn't validate if address ${address} was proxy due to ${err}`);
+        reject(false);
+      }
+    });
+  });
 }
 
-(async () => {
-  const result = await isAddressAProxy("0x25cf21ce1f31fd5fb77490c2160a6ed3ab70c909");
-  console.log(result);
-})();
+function listen() {
+  console.log("Web3 version:",web3.version.api);
+  console.log("-----------------------------------------------");
+  console.log("Network: ", network);
+  console.log("Market Address: ", marketAddress);
+  console.log("Proxy Factory Address: ", proxyFactoryAddress);
+  console.log("-----------------------------------------------");
+  console.log("Listening for incoming trades...");
+
+  market.LogTake({}, async (error, trade) => {
+    const taker = trade.args.taker;
+    const isProxy = await isAddressAProxy(taker);
+
+    console.log('Address:',taker);
+    console.log('Is Proxy', isProxy);
+  })
+}
+
+listen();
+
+
+// This is a hack to keep the process alive.
+// Refer to: https://stackoverflow.com/questions/23622051/how-to-forcibly-keep-a-node-js-process-from-terminating
+process.stdin.on("data", () => {
+});
