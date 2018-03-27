451de616d8ec5585d83eda2fbdc3685253927b49
Pex <georgi.h.georgieff@gmail.com>
2018-03-27 09:45:20 +0200

    Simple check if an address is proxy. Using hardcoded address.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const config = require("./config.json");
+const proxyFactoryAbi = require("./abi/dsProxyFactory.json").abi;
 const Web3 = require("web3");
 
 const PRIVATE_NETWORK = "private";
@@ -17,3 +18,17 @@ if(!proxyFactoryAddress) {
   console.log("Missing Proxy Factory address. Check config.json!");
   process.exit(1);
 }
+
+const web3 = new Web3(provider);
+const proxyFactoryInstance = new web3.eth.Contract(proxyFactoryAbi,proxyFactoryAddress);
+
+console.log(proxyFactoryInstance);
+
+function isAddressAProxy(address){
+   return proxyFactoryInstance.methods.isProxy(address).call();
+}
+
+(async () => {
+  const result = await isAddressAProxy("0x25cf21ce1f31fd5fb77490c2160a6ed3ab70c909");
+  console.log(result);
+})();
